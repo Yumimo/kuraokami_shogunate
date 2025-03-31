e80db2902f2cd9d7bbc7cb12cbf94b8e04855294
Yumimo <ejoshua.naranjo@gmail.com>
2025-03-31 15:49:09 +0800

    setup folders and add packages

---
+++ Assets/_Contents/Scripts/Input/InputReader.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Kuraokami
+{
+    [CreateAssetMenu(fileName = "Input", menuName = "Scriptable Objects/Input")]
+    public class InputReader : ScriptableObject, InputSystem_Actions.IPlayerActions
+    {
+        public void OnMove(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnPrevious(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnNext(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+
+        }
+    }
+
+}
